70f60799fe1052519e45137d017da8876a2911bc
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-09-21 20:47:37 +0200

    Add translations

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import __ from './i18n';
+
+
 export default function () {
-    return require('../package.json');
+    return {
+        ...require('../package.json'),
+        title: __('package.title'),
+        description: __('package.description')
+    };
 }
