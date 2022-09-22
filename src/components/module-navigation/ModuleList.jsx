43b68a08b7a31058e987b9b438d043df5f3d9676
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-09-22 08:47:40 +0200

    Highlight current item

--- src/components/module-navigation/ModuleList.jsx
+++ src/components/module-navigation/ModuleList.jsx
@@ -6,6 +6,7 @@ import { router, api } from '@artevelde-uas/canvas-lms-app';
 
 export default () => {
     const [modules, setModules] = useState();
+    const [itemSequence, setItemSequence] = useState();
 
     useEffect(() => {
         const params = router.getParams();
@@ -13,16 +14,27 @@ export default () => {
         api.get(`/courses/${params.courseId}/modules`, {
             per_page: 100
         }).then(setModules);
+
+        api.get(`/courses/${params.courseId}/module_item_sequence`, {
+            asset_id: params.module_item_id,
+            asset_type: 'ModuleItem'
+        }).then(itemSequence => {
+            setItemSequence(itemSequence.items[0]);
+        });
     }, []);
 
     return (
         <View>
             <ul>
-                {modules && modules.map(module => (
+                {(modules && itemSequence) && modules.map(module => (
                     <li
                         key={module.id}
                     >
-                        {module.name}
+                        {module.id === itemSequence.current.module_id ? (
+                            <b>{module.name}</b>
+                        ) : (
+                            <a>{module.name}</a>
+                        )}
                     </li>
                 ))}
             </ul>
