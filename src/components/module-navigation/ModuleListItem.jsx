f772c2d6326d15b7c346008a52240f28958b78b6
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-09-24 21:34:41 +0200

    Make `expanded` property controled

--- src/components/module-navigation/ModuleListItem.jsx
+++ src/components/module-navigation/ModuleListItem.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToggleDetails } from '@instructure/ui-toggle-details';
 
 
 export default ({ module, currentItem }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    useEffect(() => {
+        setExpanded(module.id === currentItem?.module_id);
+    }, [currentItem]);
+
+    function handleToggle(event, expanded) {
+        setExpanded(expanded);
+    }
+
     return (
         <ToggleDetails
             summary={module.name}
-            defaultExpanded={module.id === currentItem?.module_id}
+            expanded={expanded}
+            onToggle={handleToggle}
         >
             <span>TEST</span>
         </ToggleDetails>
