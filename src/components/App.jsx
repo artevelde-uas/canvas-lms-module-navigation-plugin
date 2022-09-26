85e03c03e260dd384bc7d7b98524ace2fb283793
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-09-26 13:19:21 +0200

    Add button to show navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { EmotionThemeProvider } from '@instructure/emotion';
-import { theme } from '@artevelde-uas/canvas-lms-app';
+
+import { dom, theme } from '@artevelde-uas/canvas-lms-app';
 
 import ModuleNavigationTray from './module-navigation/NavigationTray';
 
@@ -8,7 +9,23 @@ import __ from '../i18n';
 
 
 export default () => {
-    const [trayOpen, setTrayOpen] = useState(true);
+    const [trayOpen, setTrayOpen] = useState(false);
+
+    useEffect(async () => {
+        const footer = await dom.onElementReady('.module-sequence-footer-content');
+
+        footer.insertAdjacentHTML('beforeend', `
+            &nbsp;
+            <a class="btn">
+                <i class="icon-module"></i>
+                ${__('show_module_navigation')}
+            </a>
+        `);
+
+        const button = footer.lastElementChild;
+
+        button.onclick = () => { setTrayOpen(true); };
+    }, []);
 
     return (
         <EmotionThemeProvider theme={theme}>
