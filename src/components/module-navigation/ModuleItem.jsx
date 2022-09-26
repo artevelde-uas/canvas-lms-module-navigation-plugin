06c324cc3c3275113e4fa6bb56e6750e711675b0
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-09-26 12:29:57 +0200

    Add item icons

--- src/components/module-navigation/ModuleItem.jsx
+++ src/components/module-navigation/ModuleItem.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from '@instructure/ui-link';
 
+import ModuleItemIcon from './ModuleItemIcon';
+
 
 export default ({ item }) => (
     <div style={{
         paddingLeft: `${item.indent}rem`
     }}>
+        <ModuleItemIcon type={item.type} />
+        &nbsp;
         <Link href={item.html_url}>
             {item.title}
         </Link>
