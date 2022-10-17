0b57226473a3e42aeaf429a20c2ffbd0ad8291c4
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-10-18 00:46:49 +0200

    Disable locked modules

--- src/components/module-navigation/ModuleItem.jsx
+++ src/components/module-navigation/ModuleItem.jsx
@@ -2,11 +2,12 @@ import React, { Fragment } from 'react';
 import { Heading } from '@instructure/ui-heading';
 import { IconPlaySolid } from '@instructure/ui-icons';
 import { Link } from '@instructure/ui-link';
+import { Text } from '@instructure/ui-text';
 
 import ModuleItemIcon from './ModuleItemIcon';
 
 
-export default ({ item, isCurrentItem }) => (
+export default ({ item, isCurrentItem, isLocked }) => (
     <div style={{
         paddingLeft: `${item.indent}rem`
     }}>
@@ -27,6 +28,10 @@ export default ({ item, isCurrentItem }) => (
                         &nbsp;
                         <em><strong>{item.title}</strong></em>
                     </Fragment>
+                ) : isLocked ? (
+                    <Text color='secondary'>
+                        {item.title}
+                    </Text>
                 ) : (
                     <Link href={item.html_url}>
                         {item.title}
