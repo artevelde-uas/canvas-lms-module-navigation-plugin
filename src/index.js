5ca41928ea1596d87821d56f0628a945729c0d6c
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2022-11-21 20:56:42 +0100

    Add navigation to modules page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 
 import { dom, router } from '@artevelde-uas/canvas-lms-app';
 
+import ModulesNavigation from './components/modules-navigation';
 import ModuleItemNavigation from './components/module-item-navigation';
 
 import __ from './i18n';
 
 
 export default function () {
+    // Render on the modules page
+    router.onRoute('courses.modules', () => {
+        // Create the container element
+        const container = document.createDocumentFragment();
+
+        // Render the component
+        ReactDOM.render(React.createElement(ModulesNavigation), container);
+    });
+
+    // Render on each module item page
     router.onRoute('courses.*', () => {
         dom.onElementReady('.module-sequence-footer').then(() => {
             // Create the container element
